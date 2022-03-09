7dd2f30ad90af30e928a3be95a479e17f30590fd
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-03-09 23:02:50 +0300

    added new files

--- scripts.js
+++ scripts.js
@@ -196,7 +196,7 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];*/
 
  $(document).ready(function(){
      $(".clickable").click(function(){
-         $("#picture-showing").fadeIn();
-         $("#picture-hidden").fadeOut();
+         $("#picture-showing").fadeToggle();
+         //$("#picture-hidden").fadeOut();
      });
  });
