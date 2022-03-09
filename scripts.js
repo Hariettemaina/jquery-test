eb95bde6aa7ba3b9de780bf62f4d0f2c5dcbd19d
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-03-09 12:31:50 +0300

    added picture a p tag and styles in css

--- scripts.js
+++ scripts.js
@@ -174,4 +174,29 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];*/
 //console.log(combined);
 
 
- 
+//jquery practice
+
+ /*$(document).ready(function(){
+     $("h1").click(function(){
+         alert("This is a header.");
+     });
+     $("p").click(function(){
+         alert("This is a paragraph.");
+     });
+     $("img").click(function(){
+         alert("This is an image.");
+     });
+ });*/
+
+ /*$(document).ready(function() {
+     $("p").click(function() {
+         $("img").show();
+     });
+ });*/
+
+ $(document).ready(function(){
+     $(".clickable").click(function(){
+         $("#picture-showing").fadeIn();
+         $("#picture-hidden").fadeOut();
+     });
+ });
